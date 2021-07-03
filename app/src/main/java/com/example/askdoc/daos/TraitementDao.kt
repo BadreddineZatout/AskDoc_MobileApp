ab1affc8217f0e66c0e4b840efd8d077a910a61e
BadreddineZatout <hb_zatout@esi.dz>
2021-07-03 15:26:05 +0100

    offline treatments

--- app/src/main/java/com/example/askdoc/daos/TraitementDao.kt
+++ app/src/main/java/com/example/askdoc/daos/TraitementDao.kt
@@ -1,7 +1,12 @@
 package com.example.askdoc.daos
 
-import androidx.room.Dao
+import androidx.room.*
+import com.example.askdoc.models.Traitement
 
 @Dao
 interface TraitementDao {
+    @Query("select * from Traitement where patientId=:id")
+    fun getTreatments(id:Int):List<Traitement>
+    @Insert
+    fun addTreatments(treatments:List<Traitement>)
 }
--- app/src/main/java/com/example/askdoc/endpoints/Endpoint.kt
+++ app/src/main/java/com/example/askdoc/endpoints/Endpoint.kt
@@ -3,12 +3,20 @@ package com.example.askdoc.endpoints
 import com.example.askdoc.models.Auth
 import com.example.askdoc.models.Doctor
 import com.example.askdoc.models.Patient
+import com.example.askdoc.models.Traitement
 import retrofit2.Call
 import retrofit2.http.*
 
 interface Endpoint {
     @POST("patient/Auth")
     fun AuthPatient(@Body auth:Auth):Call<Patient>
+
     @GET("doctor")
     fun getMedecins():Call<List<Doctor>>
+
+    @GET("traitement/offline")
+    fun getAllTreaitementOffline():Call<List<Traitement>>
+
+    @PUT("traitement/offline")
+    fun updateOffline():Call<String>
 }
