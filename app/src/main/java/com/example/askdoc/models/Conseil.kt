86118bd53a81b8eb5bf9a7560fd866d34cfba095
dbouidaine <hd_bouidaine@esi.dz>
2021-07-06 06:44:06 +0100

    test

--- app/src/main/java/com/example/askdoc/models/Conseil.kt
+++ app/src/main/java/com/example/askdoc/models/Conseil.kt
@@ -3,7 +3,13 @@ package com.example.askdoc.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(primaryKeys = arrayOf("doctorId", "patientId"))
+@Entity
 data class Conseil (
-    val doctorId:Int, val patientId:Int, val text:String, var isSyncronized:Int=0
-)
+    val doctorId:Int,
+    val patientId:Int,
+    val text:String,
+    var isSyncronized:Int=0
+){
+    @PrimaryKey(autoGenerate = true)
+    var counselId:Int?=null
+}
